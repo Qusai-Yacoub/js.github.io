9014560e8dc2da2630da9158dca3292afef2954b
Qusai-Yacoub <148435160+Qusai-Yacoub@users.noreply.github.com>
2024-01-14 22:59:07 +0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,9 @@
+function myf(){
+
+    var inp= Number(document.getElementById("i1").value) 
+for( x=1;x<=inp;x++){
+
+document.getElementById("p1").innerHTML
++=
+("The Number is "+ " : " + x + "<br>") }}
+
